6697795d4d5a28b3fe27abbfa017dcbd144ec575
Gallay Lajos <gallay.lajos@live.com>
2017-12-04 15:51:40 +0100

    refactor(project): updated dependencies and APIs (sn-client, sn-redux)

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { NgRedux } from '@angular-redux/store';
 import { Actions } from 'sn-redux';
-import { Content, ContentTypes } from 'sn-client-js';
+import { Content, ContentTypes, Repository } from 'sn-client-js';
 import { Enums } from 'sn-client-js';
 
 export interface IAppState {
@@ -11,10 +11,10 @@ export interface IAppState {
 @Component({
   selector: 'app-add-todo',
   template: `
-    <md-input-container>
-      <input #taskName mdInput placeholder="What needs to be done?">
-    </md-input-container>
-    <button (click)="createTask(taskName.value)" md-button>Add todo!</button>
+    <mat-form-field>
+      <input #taskName matInput placeholder="What needs to be done?">
+    </mat-form-field>
+    <button (click)="createTask(taskName.value)" mat-button>Add todo!</button>
   `,
   styles: [`
 
@@ -24,15 +24,23 @@ export class AddTodoComponent implements OnInit {
   @Input() state: IAppState;
   @Input() path: string;
 
-  constructor(private ngRedux: NgRedux<IAppState>) { }
+  constructor(private ngRedux: NgRedux<IAppState>, private repository: Repository.SnRepository) { }
 
   ngOnInit() {
   }
 
   createTask(title: string) {
-    this.ngRedux.dispatch(Actions.CreateContent(this.path, ContentTypes.Task, {
+    // const c = this.repository.CreateContent({
+    //   Name: title,
+    //   DueDate: null,
+    //   Path: this.path,
+    //   Type: 'Task'
+    // }, ContentTypes.Task);
+    this.ngRedux.dispatch(Actions.CreateContent({
       Name: title,
-      DueDate: null
+      DueDate: null,
+      Path: this.path,
+      Type: 'Task'
     }));
   }
 }
